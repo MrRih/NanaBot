a5ce1c4232ac8aa8a281602b8bcfda76fb79a95a
Clawn <86503600+MrRih@users.noreply.github.com>
2021-07-20 08:00:59 +0700

    Update __main__.py

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -56,7 +56,7 @@ from DaisyX import (
 
 # needed to dynamically load modules
 # NOTE: Module order is not guaranteed, specify that in the config file!
-from DaisyX.modules import ALL_MODULES, OWNER, SUPPORT_GROUP, UPDATES_CHANNEL
+from DaisyX.modules import ALL_MODULES
 from DaisyX.modules.helper_funcs.alternate import typing_action
 from DaisyX.modules.helper_funcs.chat_status import is_user_admin
 from DaisyX.modules.helper_funcs.misc import paginate_modules
@@ -72,11 +72,11 @@ RAWAT GROUP ELU, BIAR ELU NYA KAGAK CAPEK!
 
 buttons = [
     [
-        InlineKeyboardButton(text="👮OWNER👮", url=f"https://t.me/{OWNER}"),
-        InlineKeyboardButton(text="💬GROUP💬", url="https:/t.me/{SUPPORT_GROUP}")],
+        InlineKeyboardButton(text="👮OWNER👮", url=f"https://t.me/tehmanisangett"),
+        InlineKeyboardButton(text="💬GROUP💬", url="https:/t.me/Samalete")],
     [
         InlineKeyboardButton(text="❓ BANTUAN ❓", callback_data="help_back"),
-        InlineKeyboardButton(text="📣CHANNEL📣", url="https://t.me/{UPDATES_CHANNEL}")
+        InlineKeyboardButton(text="📣GROUP 2📣", url="https://t.me/Musikzamm")
     ],
     [
         InlineKeyboardButton(
